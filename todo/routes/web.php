b41e9ba7176a783c8213d076bdd3623af93d110f
Ashish khadka <khadkaashish50@gmail.com>
2024-08-12 07:41:56 +0545

    latest

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -3,18 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend.main');
 });
 
 // First
 Route::get('/contact', function(){
-    return view('contact');
+    return view('backend.pages.contact');
 
 });
 
 // Second
-Route::get('/product', function(){
-    return view('product.product');
+Route::get('/about', function(){
+    return view('backend.pages.about');
+});
+
+// third
+Route::get('/home', function(){
+    return view('backend.home.home');
 });
 
 Route::get('/product/list', [\App\Http\Controllers\productController::class, 'index']);
